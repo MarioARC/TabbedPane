1b887cac351db93154e3c752bcc3454611b5c429
Mario Castelo <91093467@g.mvusd.net>
2021-03-01 19:19:03 +0000

    Initial commit

---
+++ JTabbedPaneFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class JTabbedPaneFrame extends JFrame
+{
+  public JTabbedPaneFrame()
+  {
+    super("JTabbedPane Demo");
+  } // end default and only constructor
+
+} // end class
---
+++ Main.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Main
+{
+  public static void main (String[] args)
+  {
+    JTabbedPaneFrame tpf = new JTabbedPaneFrame();
+    
+    tpf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    tpf.setSize(450, 400);
+    tpf.setVisible(true);
+  } // end main method
+} //  end Main class
